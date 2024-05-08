12a4ee238b7658a55d6e0b73228d1dbecec13586
André de Paula Terceiro <andre@terceiro.com.br>
2024-05-08 13:10:14 -0300

    A test of the scan function

---
+++ tests/manuals/scan.r
@@ -0,0 +1,16 @@
+# R program to illustrate
+# taking input from the user
+ 
+# double input using scan()
+d = scan(what = double())
+ 
+# string input using 'scan()'
+s = scan(what = " ")
+ 
+# character input using 'scan()'
+c = scan(what = character())
+ 
+# print the inputted values
+print(d) # double
+print(s) # string
+print(c) # character
