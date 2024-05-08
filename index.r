332420d7c9f522b26e9fecf260fa5a0e24937e7b
André de Paula Terceiro <andre@terceiro.com.br>
2024-05-08 14:01:41 -0300

    Finishing

--- index.r
+++ index.r
@@ -1,4 +1,5 @@
-readUserInput <- function() {
+# Reading the user choice
+readUserChoice <- function() {
     userChoice = ""
 
     while (userChoice != "p" && userChoice != "r" && userChoice != "s") {
@@ -9,7 +10,39 @@ readUserInput <- function() {
     return(userChoice);
 }
 
+readComputerChoice <- function() {
+    computerChoice <- as.integer(runif(n=1, min=1, max=3.9999999999))
+
+    print (computerChoice)
+    if (computerChoice == 1) {
+        return("paper");
+    } else if (computerChoice == 2) {
+        return("rock");
+    }
+    return("scissors");
+}
+
+getCompleteStringOfChoice <- function(choice) {
+    if (choice == "p") {
+        return("paper");
+    } else if (choice == "r") {
+        return("rock");
+    }
+    return("scissors");
+}
+
 print("Jankenpo!");
 print("=========");
-userInput = readUserInput();
+userChoice = getCompleteStringOfChoice(readUserChoice());
+computerChoice = readComputerChoice();
 
+if ((userChoice == "paper" && computerChoice == "rock") || (userChoice == "rock" && computerChoice == "scissors") || (userChoice == "scissors" && computerChoice == "paper")) {
+    print("You win!")
+} else if (userChoice == computerChoice) {
+    print("Draw!")
+} else {
+    print("Computer win!")
+}
+
+print(paste("User choice: ",  userChoice))
+print(paste("Computer choice: ", computerChoice))
